9ea3026d767dbeb4c3d50c71578cc75f08cd7925
xzahra33 <xzahra33@vutbr.cz>
2024-10-18 08:11:21 +0200

    [Repository/Form] Add Repository and IRepository [Model/Form] Add List and Detail Model [Facade/Form] Add IFacade

---
+++ Forms.Api.DAL.EF/Repositories/FormRepository.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using Forms.Api.DAL.Common.Entities;
+using Forms.Api.DAL.Common.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forms.Api.DAL.EF.Repositories;
+
+public class FormRepository : RepositoryBase<FormEntity>, IFormRepository
+{
+    private readonly IMapper _mapper;
+    
+    protected FormRepository(FormsDbContext context, IMapper mapper) : base(context)
+    {
+        this._mapper = mapper;
+    }
+
+    public override FormEntity? GetById(Guid id)
+    {
+        return context.Forms
+            .Include(f => f.User)
+            .Include(f => f.Questions)
+            .FirstOrDefault(f => f.Id == id);
+    }
+    
+    
+    public override Guid? Update(FormEntity form)
+    {
+        if (Exists(form.Id))
+        {
+            var existingForm = context.Forms
+                .Include(f => f.User)
+                .Include(f => f.Questions)
+                .Single(u => u.Id == form.Id);
+            
+            _mapper.Map(form, existingForm);
+            
+            context.Forms.Update(existingForm);
+            context.SaveChanges();
+            
+            return existingForm.Id;
+        }
+
+        return null;
+    }
+}
