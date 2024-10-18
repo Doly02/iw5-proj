95df37ba26c203fdc7653f724f71256028ba9392
xzahra33 <xzahra33@vutbr.cz>
2024-10-18 08:12:58 +0200

    [Repository/Response] Add IRepository [Model/Response] Add List and Detail Model

---
+++ Common/Forms.Common.Models/Response/ResponseDetailModel.cs
@@ -0,0 +1,13 @@
+using Forms.Api.DAL.Common.Entities;
+
+namespace Forms.Common.Models.Response;
+
+public record ResponseDetailModel : IWithId
+{
+    public required Guid Id { get; init; }
+    public required Guid UserId { get; set; }  // todo treba?
+    public required Guid QuestionId { get; set; }  // todo treba?
+    public required UserEntity User { get; set; }
+    public required QuestionEntity Question { get; set; }
+    public List<string?>? UserResponse { get; set; }
+}
