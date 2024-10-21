0f35d273dc932854fa58ee2e45248f373d9c5219
Tomáš Dolák <tomas.dolak@outlook.cz>
2024-10-21 21:19:14 +0200

    [Response] Updates DetailModel & ListModel.

--- Common/Forms.Common.Models/Response/ResponseDetailModel.cs
+++ Common/Forms.Common.Models/Response/ResponseDetailModel.cs
@@ -5,9 +5,10 @@ namespace Forms.Common.Models.Response;
 public record ResponseDetailModel : IWithId
 {
     public required Guid Id { get; init; }
-    public required Guid UserId { get; set; }  // todo treba?
-    public required Guid QuestionId { get; set; }  // todo treba?
+    
     public required UserEntity User { get; set; }
+    
     public required QuestionEntity Question { get; set; }
+    
     public List<string?>? UserResponse { get; set; }
 }
