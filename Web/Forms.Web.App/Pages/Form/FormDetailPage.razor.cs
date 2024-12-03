29d48b009e39b3071c48a8086438d8824f1cdf71
Tomáš Dolák <tomas.dolak@outlook.cz>
2024-12-03 21:39:33 +0100

    [Forms] Updates Question List in Forms.

--- Web/Forms.Web.App/Pages/Form/FormDetailPage.razor.cs
+++ Web/Forms.Web.App/Pages/Form/FormDetailPage.razor.cs
@@ -1,6 +1,7 @@
 using Forms.Common.Models.Form;
 using Forms.Web.BL.Facades;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Forms.Web.App.Pages
 {
@@ -9,14 +10,71 @@ namespace Forms.Web.App.Pages
         [Inject]
         private FormFacade FormFacade { get; set; } = null!;
 
+        //[Inject]
+        //private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
         [Parameter]
         public Guid Id { get; set; }
 
+        public FormDetailModel Data { get; set; } = null!;
         private FormDetailModel? Form { get; set; }
+        private Guid CurrentUserId { get; set; }
+        public EventCallback OnModification { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
+            /*
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (!user.Identity?.IsAuthenticated ?? true)
+            {
+                throw new UnauthorizedAccessException("Uživatel není přihlášen.");
+            }
+
+            var userIdClaim = user.FindFirst("Id");
+            if (userIdClaim == null)
+            {
+                throw new InvalidOperationException("Nelze načíst ID uživatele.");
+            }
+            CurrentUserId = Guid.Parse(userIdClaim.Value);
+            */
+
+            // Inicializace dat formuláře
             Form = await FormFacade.GetByIdAsync(Id);
+            // Data = Form ?? GetNewFormModel(CurrentUserId);
         }
+
+        public async Task Save()
+        {
+            await FormFacade.SaveAsync(Data);
+            Data = GetNewFormModel(CurrentUserId);
+            await NotifyOnModification();
+        }
+
+        public async Task Delete()
+        {
+            await FormFacade.DeleteAsync(Id);
+            await NotifyOnModification();
+        }
+
+        private async Task NotifyOnModification()
+        {
+            if (OnModification.HasDelegate)
+            {
+                await OnModification.InvokeAsync(null);
+            }
+        }
+
+        private FormDetailModel GetNewFormModel(Guid userId)
+            => new()
+            {
+                Id = Guid.NewGuid(),
+                DateOpen = DateTime.Now,
+                DateClose = DateTime.Now,
+                Name = string.Empty,
+                Description = string.Empty,
+                UserId = new Guid(),
+            };
     }
 }
