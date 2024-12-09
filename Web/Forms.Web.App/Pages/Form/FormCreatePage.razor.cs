546e1ba414055c92bcdf74b3fba6cafb53e9836b
xzahra33 <xzahra33@vutbr.cz>
2024-12-09 16:50:06 +0100

    [Form Page] Create page

--- Web/Forms.Web.App/Pages/Form/FormCreatePage.razor.cs
+++ Web/Forms.Web.App/Pages/Form/FormCreatePage.razor.cs
@@ -3,30 +3,26 @@ using Forms.Common.Models.Form;
 using Forms.Common.Models.Question;
 using Forms.Web.BL.Facades;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Forms.Web.App.Pages
 {
     public partial class FormCreatePage
     {
         [Inject]
+        private new AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+        [Inject]
         private NavigationManager NavigationManager { get; set; } = null!;
 
         [Inject]
         private FormFacade FormFacade { get; set; } = null!;
+        private string? NewAnswer { get; set; } = string.Empty;
+        private string? ErrorMessage { get; set; }
+        private static Guid CurrentUserId { get; set; }
 
-        private FormDetailModel Form { get; set; } = new()
-        {
-            Id = Guid.NewGuid(),
-            Name = string.Empty,
-            Description = string.Empty,
-            DateOpen = DateTime.Now,
-            DateClose = DateTime.Now.AddDays(7),
-            Questions = new List<Forms.Common.Models.Question.QuestionDetailModel>(),
-            UserId = Guid.Parse("b3f62b8b-291c-42e5-b8d9-3afbb3276dca")   //TODO upravit podla aktualne prihlaseneho usera
-
-        };
+        private FormDetailModel? Form { get; set; }
         
-       private QuestionDetailModel NewQuestion { get; set; } = new()
+        private QuestionDetailModel NewQuestion { get; set; } = new()
         {
             Id = Guid.NewGuid(),
             Name = "Nová otázka",
@@ -34,15 +30,44 @@ namespace Forms.Web.App.Pages
             QuestionType = QuestionType.OpenQuestion,
             Answer = new List<string>()
         };
+        
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
 
-        private string? NewAnswer { get; set; } = string.Empty;
-        private string? ErrorMessage { get; set; }
+            if (!user.Identity?.IsAuthenticated ?? true)
+            {
+                throw new UnauthorizedAccessException("Uživatel není přihlášen.");
+            }
+
+            var userIdClaim = user.FindFirst("Id");
+            if (userIdClaim == null)
+            {
+                throw new InvalidOperationException("Nelze načíst ID uživatele.");
+            }
+
+            CurrentUserId = Guid.Parse(userIdClaim.Value);
+            
+            Console.WriteLine($"User Id: {CurrentUserId}");
+            
+            Form = new FormDetailModel
+            {
+                Id = Guid.NewGuid(),
+                Name = string.Empty,
+                Description = string.Empty,
+                DateOpen = DateTime.Now,
+                DateClose = DateTime.Now.AddDays(7),
+                Questions = new List<QuestionDetailModel>(),
+                UserId = CurrentUserId
+            };
+        }
 
         private async Task SaveForm()
         {
             try
             {
-                await FormFacade.SaveAsync(Form);
+                if (Form != null) await FormFacade.SaveAsync(Form);
                 NavigationManager.NavigateTo("/forms");
             }
             catch (Exception)
@@ -60,7 +85,7 @@ namespace Forms.Web.App.Pages
                 return;
             }
 
-            Form.Questions.Add(new QuestionDetailModel
+            Form?.Questions.Add(new QuestionDetailModel
             {
                 Id = Guid.NewGuid(),
                 Name = NewQuestion.Name,
@@ -83,19 +108,19 @@ namespace Forms.Web.App.Pages
                 return;
             }
 
-            question.Answer.Add(NewAnswer);
+            question.Answer?.Add(NewAnswer);
             NewAnswer = string.Empty;
             ErrorMessage = null;
         }
 
         private void RemoveAnswer(QuestionDetailModel question, string answer)
         {
-            question.Answer.Remove(answer);
+            question.Answer?.Remove(answer);
         }
 
         private void RemoveQuestion(QuestionDetailModel question)
         {
-            Form.Questions.Remove(question);
+            Form?.Questions.Remove(question);
         }
 
         private void ResetNewQuestion()
